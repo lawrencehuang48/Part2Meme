685fa9d81f9283c510cc7d8242241c9354b4d0c6
Lawrence <lawrencehuang48@gmail.com>
2018-11-18 02:49:10 +1300

    First commit

---
+++ Data/Part2Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Part2.Models
+{
+    public class Part2Context : DbContext
+    {
+        public Part2Context (DbContextOptions<Part2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Part2.Models.MemeItem> MemeItem { get; set; }
+    }
+}
